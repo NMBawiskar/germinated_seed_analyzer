Index error in branchClass resolved. Breakpoint after addition of new hyp points was showing wrong results. resolved.

req_classes/dataAnalysis.py
@@ -1,31 +1,3 @@
-
-
-
-"""  ### FOr each picture
-Penalization = n(dead seeds) * (50/n(total))
-Where:
-n = total seeds
-n(mortas) = dead seeds (seeds that didn't germinated)
-
-
-= 2 * 50 /20 
-
-"""
-# summary.csv
-
-# fileName  penalization value
-# N1A.jpg   0.3
-# N2A.jpg   0.4
-
-
-"""
-ti = length of the analyzed seedling
-T =  average length of the entire batch(image)
-n = plants in the batch (image)
-
-seedVigorIndex = max (0,  (1 - (summation_i_1_to_n( abs(Ti - T) / n * T) * 1000 )   -   Penalization   ))
-
-"""
 from .contour_processor import Seed
 from proj_settings import MainSettings, SeedHealth
 import json
@@ -69,8 +41,6 @@ class BatchAnalysisNew:
         self.std_deviation = 0
         self.germination_percent = 0
 
-        
-
         self.recalculate_all_metrics()
 
 
@@ -82,7 +52,6 @@ class BatchAnalysisNew:
             self.dict_settings = json.load(f)
 
 
-                
 
         self.get_seed_class_count()
 
@@ -97,10 +66,13 @@ class BatchAnalysisNew:
         self.dead_seed_count, self.abnormal_seed_count, self.germinated_seed_count = 0,0,0
 
         for seedObj in self.seedObjList:
+            
             if seedObj.seed_health == SeedHealth.DEAD_SEED:
                 self.dead_seed_count+=1
+                
             elif seedObj.seed_health == SeedHealth.ABNORMAL_SEED:
                 self.abnormal_seed_count+=1
+                
             elif seedObj.seed_health == SeedHealth.NORMAL_SEED:
                 self.germinated_seed_count+=1
 
@@ -112,7 +84,10 @@ class BatchAnalysisNew:
 
 
     def calc_penalization(self):
-        self.penalization = self.dead_seed_count * 50 / self.n_total_seeds_in_image
+
+        # self.penalization = self.dead_seed_count * 50 / self.n_total_seeds_in_image # ERRADO!
+        self.penalization = self.dead_seed_count *( 50 / self.n_total_seeds_in_image) # Correto!
+
         return self.penalization
 
 
@@ -153,31 +128,23 @@ class BatchAnalysisNew:
             self.avg_root_length_pixels = np.round(np.sum(self.list_root_lengths_pixels) /  len(self.seedObjList), 2)
         except Exception as e:
             print(traceback.format_exc())
-            
-
-
-
-    def calculate_uniformity_or_Uniformidade(self):
-        # seedVigorIndex = max (0,  (1 - (summation_i_1_to_n( abs(Ti - T) / n * T) * 1000 )   -   Penalization   ))     
-
-        # sortedList = sorted(self.list_total_seed_lengths)
-        # centralIndex = (self.n_total_seeds_in_image - 1) // 2
     
-        # if self.n_total_seeds_in_image %2 ==0:
-        #     medianSeedLength = (sortedList[centralIndex] + sortedList[centralIndex+1] ) /2
-        # else:
-        #     medianSeedLength = sortedList[centralIndex]
-
-               
-        
+    def calculate_std_deviation_and_other(self):
+        self.std_deviation = np.round(np.std(self.list_total_seed_lengths_cm),2)
+        self.germination_percent =  np.round(self.germinated_seed_count/ self.n_total_seeds_in_image * 100, 2)
+            
+    
+    def calculate_uniformity_or_Uniformidade(self):
         abs_sum = np.sum([np.abs(l_seed - self.avg_total_length_pixels) for l_seed in self.list_total_seed_lengths_pixels])
         uni_ = (1 -  (abs_sum / (self.n_total_seeds_in_image * self.avg_total_length_pixels))) * 1000 - self.penalization 
-
         self.uniformity = int(max(0, uni_))
-
+        #"avg_total_length_settings" ----> mudar para "avg_total_length", não é de settings, é o tamanho médio das plântulas na imagem.
+        # Uniformidade mede com o Comp.médio das plantulas na imagem.
+        # Crescimento que usa o fato 12.05.
 
     def calculate_growth_or_Crescimento(self):
         """ growth (or Crescimento) = avg of seed lengths(rad+hyp) / max length * 1000
+        COMPRIMENTO MÉDIO DA PLÂNTULA INTEIRA / COMPRIMENTO MÉDIO (USUÁRIO) * 1000
         """
         ## wrong formula before       
         # self.growth = self.avg_total_length / max(self.list_total_seed_lengths) * 1000
@@ -191,18 +158,15 @@ class BatchAnalysisNew:
 
         # self.growth = min(self.avg_root_length_pixels* self.pr_ + self.avg_hypocotyl_length_pixels *self.ph_, 1000)
         self.growth = np.round((self.avg_total_length_cm/ self.avg_total_length_Settings) * 1000)
-
+        
+        # The lenghts are in pixels.
+        # 12.05 is the user given lenght, wich correlates 99% to the Vigor-S results.
         
     def calculate_seed_vigor_index(self):
+
         """ Vigor = Pc * growth (or Crescimento) + Pu * Uniformity (or Uniformidade)"""
         self.Pc = self.dict_settings['weights_factor_growth_Pc']
         self.Pu = self.dict_settings['weights_factor_uniformity_Pu']
         self.seed_vigor_index = np.round(self.Pc * self.growth + self.Pu * self.uniformity)
 
-    def calculate_std_deviation_and_other(self):
-        self.std_deviation = np.round(np.std(self.list_total_seed_lengths_cm),2)
-        self.germination_percent =  np.round(self.germinated_seed_count/ self.n_total_seeds_in_image * 100, 2)
-
-
-
-
+    

req_classes/pixel_to_cm.py
@@ -1,9 +1,9 @@
-
-
 import cv2
 import numpy as np
 
-def get_pixel_to_cm(img, checkerboard_size):
+#------------------------------ NEW WORKING CODE -------------------------------------------
+
+def get_pixel_to_cm(img, checkerboard_size = (28,20)):
     
     # Load the image
     # img = cv2.imread(img)
@@ -34,14 +34,10 @@ def get_pixel_to_cm(img, checkerboard_size):
         average_square_size_pixels = np.mean(square_size_pixels)
         # Since the size of a square in the real world is 1cm, our conversion factor from pixels to cm is then:
         pixel_per_cm = 1 / average_square_size_pixels
+        pixel_per_cm = np.round( 1/pixel_per_cm)
         print(f"Conversion factor: {pixel_per_cm} cm/pixel") # = 0.021289622730154698
         print(f"1cm is equal to:", (1/pixel_per_cm))         # = 46.97124099731445 
         return pixel_per_cm
     else:
         print("Could not find chessboard corners")
         return None
-
-# # Example usage
-# pixels_to_cm = compute_conversion_factor('001 - 48px_1cm.jpg', (28, 20))
-# print(f"Conversion factor: {pixels_to_cm} cm/pixel")
-# print(f"1cm is equal to:", (1/pixels_to_cm))

req_classes/seedEditor.py
@@ -11,22 +11,11 @@ from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import QApplication
-# def QImageToCvMat(incomingImage):
-#     '''  Converts a QImage into an opencv MAT format  '''
-
-#     incomingImage = incomingImage.convertToFormat(QtGui.QImage.Format.Format_RGBA8888)
-
-#     width = incomingImage.width()
-#     height = incomingImage.height()
-
-#     ptr = incomingImage.bits()
-#     ptr.setsize(height * width * 4)
-#     arr = np.frombuffer(ptr, np.uint8).reshape((height, width, 4))
-#     return arr
 
 class CanvasLabel(QLabel):
     def __init__(self, parent=None):
         super().__init__(parent)
+        
         self.text = "Custom Canvas"
         self.background_color = QColor(0, 255, 0)
         self.x = 50
@@ -48,12 +37,9 @@ class CanvasLabel(QLabel):
         self.last_x, self.last_y = None, None
         self.pen_draw_color = [0,255,0]
         self.pen_thickness= 2
-        self.pen_thickness_eraser = 10
+        self.pen_thickness_eraser = 20
         self.pen_color_mask = QtGui.QColor('black')
-
         self.setCursor(Qt.CursorShape.CrossCursor)
-        
-  
         self.updated_img = None
 
     def apply_cv2_image(self, imgcv2):
@@ -90,15 +76,15 @@ class CanvasLabel(QLabel):
         # print("ev", a0.x(), a0.y())
         
         if self.last_x is None:
-            self.last_x = int((a0.x()) / self.w * self.imgW) 
-            self.last_y = int((a0.y()) / self.h * self.imgH) 
+            self.last_x = int((a0.x()) / self.w * self.imgW)
+            self.last_y = int((a0.y()) / self.h * self.imgH)
                       
             return # ignore the first frame
 
 
-        self.drawX = int((a0.x()) / self.w * self.imgW) 
+        self.drawX = int((a0.x()) / self.w * self.imgW)
         self.drawY = int((a0.y()) / self.h * self.imgH)
-
+        
         
 
         if self.breakPointActive:
@@ -226,15 +212,15 @@ class SeedEditor(QWidget):
         loadUi(r'UI_files\seed_editor.ui', self)
         self.mainUi = mainUi
         self.setWindowIconText("Seed ")
-        self.btngroup1 = QtWidgets.QButtonGroup()
-
-
-        self.eraserActive = False
-        self.breakPointActive = False
-        self.seedObj:Seed = None
+        
+        #_________ SEED RELATED ____________________________________________
+        self.seedObj: Seed = None
         self.seedNo = 0
         
-
+        
+        #_________ CHECK BUTTONS ____________________________________________
+        self.btngroup1 = QtWidgets.QButtonGroup()
+        
         self.btngroup1.addButton(self.radioBtnNormalSeed)
         self.btngroup1.addButton(self.radioBtnAbnormalSeed)
         self.btngroup1.addButton(self.radioBtnDeadSeed)
@@ -243,43 +229,45 @@ class SeedEditor(QWidget):
         self.radioBtnNormalSeed.toggled.connect(self.checkRadio)
         self.radioBtnAbnormalSeed.toggled.connect(self.checkRadio)
         self.radioBtnDeadSeed.toggled.connect(self.checkRadio)
-
+        
         self.radioBtnNormalSeed.setShortcut('n')
         self.radioBtnAbnormalSeed.setShortcut('a')
         self.radioBtnDeadSeed.setShortcut('d')
-
+        
+        #_________ EDITION BUTTONS____________________________________________
+        self.btnEraser.clicked.connect(self.use_eraser)
+        self.btnPoint.clicked.connect(self.use_breakPoint)
+        self.btnDrawHyp.clicked.connect(self.use_pen_hyp)
         self.btnEraser.setShortcut('e')
         self.btnPoint.setShortcut('b')
         self.btnDrawHyp.setShortcut('1')
         self.btnDrawRoot.setShortcut('2')
-        # self.btnPen.setShortcut('p')
-
-        self.btnEraser.clicked.connect(self.use_eraser)
-        self.btnPoint.clicked.connect(self.use_breakPoint)
-        self.btnDrawHyp.clicked.connect(self.use_pen_hyp)
+        
+        self.eraserActive = False
+        self.breakPointActive = False
+        
+        
+        #_________ CANVAS RELATED ____________________________________________
         self.btnDrawRoot.clicked.connect(self.use_pen_root)
-        # self.btnPen.clicked.connect(self.use_pen)
-        # self.btnSave.clicked.connect(self.save_changes)
-
-        self.canvasMask_seededitor=None
+        self.canvasMask_seededitor = None
         self.last_x, self.last_y = None, None
-        self.pen_draw_color = [0,255,0]
-        self.pen_thickness= 2
+        self.pen_draw_color = [0, 255, 0]
+        self.pen_thickness = 2
         self.pen_color_mask = QtGui.QColor('white')
         self.imgH = 0
         self.imgW = 0
 
-        self.delta_x=390 
-        self.delta_y=30
+        self.delta_x = 390 
+        self.delta_y = 30
 
-        self.maxImgHt= 800
-        # self.label_paint_w = 301  ## To set afterwards
-        # self.label_paint_h = 781  ## To set afterwards
+        self.maxImgHt = 800
 
         self.customLabel = CanvasLabel(self)
         self.customLabel.setObjectName("img_label_mask")
         self.customLabel.seedEditor = self
-        # self.customLabel.show()
+
+        #_________ BUTTONS STYLESHEET ____________________________________________
+
         self.btnClickedStylesheet = '''background-color:rgba(216, 229, 253,50);
                         font: 63 10pt "Segoe UI Semibold";
                         color: rgb(32, 24, 255);
@@ -287,25 +275,14 @@ class SeedEditor(QWidget):
         self.btnNotClickedStylesheet = '''background-color:rgba(216, 229, 253,255);
                         font: 63 10pt "Segoe UI Semibold";
                         color: rgb(32, 24, 255);
-                        border-radius:4;'''   
+                        border-radius:4;'''
 
-        # self.lineEdit_hypocotyl_length.textChanged.connect(self.change_value_hypocotyl)
-        # self.lineEdit_root_length.textChanged.connect(self.change_value_root)
-
+        
     def save_changes(self):
         print("saving changes")
         self.customLabel.canvasMask.save('customCanvas.png')
         imgUpdated = cv2.imread('customCanvas.png')
         self.seedObj.singlBranchBinaryImg = imgUpdated
-        # print("self.lineEdit_hypocotyl_length.text()", self.lineEdit_hypocotyl_length.text(), self.lineEdit_hypocotyl_length.text().isdecimal())
-        # if len(self.lineEdit_hypocotyl_length.text())>0:
-        #     self.seedObj.hyperCotyl_length_cm = self.lineEdit_hypocotyl_length.text()
-        #     print("saving value self.seedObj.hyperCotyl_length_cm", self.seedObj.hyperCotyl_length_cm)
-        
-        # if len(self.lineEdit_root_length.text())>0:
-        #     self.seedObj.radicle_length_cm = self.lineEdit_root_length.text()
-        #     print("saving value self.seedObj.radicle_length_cm", self.seedObj.radicle_length_cm)
-
         self.mainUi.save_results_to_csv()
         self.mainUi.show_analyzed_results()
         self.mainUi.update_result_img()
@@ -313,7 +290,8 @@ class SeedEditor(QWidget):
         # cv2.imshow('self.customLabel.updated_img',self.customLabel.updated_img)
         # cv2.waitKey(1)
         self.close()
-
+    
+    #_________ TOOLS DEFINITION _______________________________________________
     def use_eraser(self):
         print("Eraser clicked")
         self.pen_color_mask = QtGui.QColor('black')
@@ -326,7 +304,7 @@ class SeedEditor(QWidget):
         self.customLabel.breakPointActive=False
         self.customLabel.eraserActive=True
 
-        self.label_img_seed_mask.setVisible(True)
+        # self.label_img_seed_mask.setVisible(True)
 
         self.btnDrawHyp.setStyleSheet(self.btnNotClickedStylesheet)
         self.btnDrawRoot.setStyleSheet(self.btnNotClickedStylesheet)
@@ -345,7 +323,7 @@ class SeedEditor(QWidget):
         self.customLabel.breakPointActive=True
         self.customLabel.eraserActive=False
 
-        self.label_img_seed_mask.setVisible(True)
+        # self.label_img_seed_mask.setVisible(True)
         print(f"Initial hyperCotyl_length_pixels , radicle_length_pixels : {self.seedObj.hyperCotyl_length_cm}, {self.seedObj.radicle_length_cm}")
       
         self.btnDrawHyp.setStyleSheet(self.btnNotClickedStylesheet)
@@ -365,7 +343,7 @@ class SeedEditor(QWidget):
         self.customLabel.breakPointActive=False
         self.customLabel.eraserActive=False
 
-        self.label_img_seed_mask.setVisible(False)
+        # self.label_img_seed_mask.setVisible(False)
 
         self.btnDrawHyp.setStyleSheet(self.btnClickedStylesheet)
         self.btnDrawRoot.setStyleSheet(self.btnNotClickedStylesheet)
@@ -385,7 +363,7 @@ class SeedEditor(QWidget):
         self.customLabel.penRootActive=True
         self.customLabel.breakPointActive=False
         self.customLabel.eraserActive=False
-        self.label_img_seed_mask.setVisible(False)
+        # self.label_img_seed_mask.setVisible(False)
         self.btnDrawHyp.setStyleSheet(self.btnNotClickedStylesheet)
         self.btnDrawRoot.setStyleSheet(self.btnClickedStylesheet)
         self.btnEraser.setStyleSheet(self.btnNotClickedStylesheet)
@@ -405,7 +383,6 @@ class SeedEditor(QWidget):
         elif self.radioBtnAbnormalSeed.isChecked():
             print("radio Abnormal is checked")
             self.seedObj.seed_health = SeedHealth.ABNORMAL_SEED
-            
         
         elif self.radioBtnDeadSeed.isChecked():
             print('radio Dead Seed is checked')
@@ -420,12 +397,10 @@ class SeedEditor(QWidget):
         imgPilMask = Image.fromarray(rgb_image.copy()).convert('RGB')        
         imMask = ImageQt(imgPilMask).copy()
         self.canvasMask_seededitor= QtGui.QPixmap.fromImage(imMask)
-        # self.label_img_seed_mask.setPixmap(self.canvasMask_seededitor)
-        # self.label_img_seed_mask.setPixmap(self.canvasMask_seededitor)
 
     def setSeedObj(self,seedObj):
         self.seedObj = seedObj
-   
+
         self.update_values()
 
         self.imgH, self.imgW = self.seedObj.singlBranchBinaryImg.shape[:2]
@@ -448,21 +423,9 @@ class SeedEditor(QWidget):
         self.customLabel.setGeometry(self.customLabel.x, self.customLabel.y, self.customLabel.w, self.customLabel.h)
         # self.customLabel.apply_cv2_image(self.seedObj.singlBranchBinaryImg)
         self.customLabel.apply_cv2_image(self.seedObj.cropped_seed_color)
-        # self.customLabel.reset_img()
 
 
-        # self.label_img_seed_mask.setGeometry(self.delta_x, self.customLabel.y, self.customLabel.w, self.customLabel.h)
-        # print('set geometry for color label')
-        # # Load cropped_seed_color mask for editing
-        # rgb_image_color = cv2.cvtColor(self.seedObj.cropped_seed_color, cv2.COLOR_BGR2RGB)
-        # imgPilMask_color = Image.fromarray(rgb_image_color).convert('RGB')        
-        # # # imMask = ImageQt(imgPilMask).copy()
-        # # # self.canvasMask_seededitor = QtGui.QPixmap.fromImage(imMask)
-        # # self.label_img_seed_mask.setPixmap(QPixmap.fromImage(ImageQt(imgPilMask_color)))
-        # print('set pixmap color')
-        
 
-        # self.display_mask()
 
     def setSeedIndex(self, seedIndex):
         self.seedNo = seedIndex + 1
@@ -475,47 +438,16 @@ class SeedEditor(QWidget):
         self.label_seed_no.setText(str(self.seedNo))
         self.label_hypocotyl_length.setText(str(self.seedObj.hyperCotyl_length_cm))
         self.label_root_length.setText(str(self.seedObj.radicle_length_cm))
-        
-        # self.lineEdit_hypocotyl_length.setText(str(self.seedObj.hyperCotyl_length_cm))
-        # self.lineEdit_root_length.setText(str(self.seedObj.radicle_length_cm))
-
 
         self.label_total_length.setText(str(self.seedObj.total_length_cm))
         self.label_seed_health.setText(self.seedObj.seed_health)
 
-        # self.setColorPixmap()
 
         ###
-        # self.mainUi.mainProcessor.batchAnalyser.recalculate_all_metrics()
+        self.mainUi.mainProcessor.batchAnalyser.recalculate_all_metrics()
         self.mainUi.show_analyzed_results()
         self.mainUi.save_results_to_csv()
         self.mainUi.update_result_img()
-
-
-
-
-    def change_value_hypocotyl(self):
-        
-        try:
-            # new_value_hypocotyl = float(str(self.lineEdit_hypocotyl_length.text()))
-            # self.seedObj.hyperCotyl_length_cm = round(new_value_hypocotyl,2)
-            # self.seedObj.radicle_length_cm =  round(self.seedObj.total_length_cm - new_value_hypocotyl,2)          
-            pass
-            # self.lineEdit_root_length.setText(str(self.seedObj.radicle_length_cm))
-        except Exception as e:
-            print(e)
-        
-    def change_value_root(self):
-        try:
-            # new_value_root = float(str(self.lineEdit_root_length.text()))
-            # self.seedObj.radicle_length_cm = round(new_value_root,2)
-            # self.seedObj.hyperCotyl_length_cm =  round(self.seedObj.total_length_cm - new_value_root, 2)          
-
-            # self.lineEdit_hypocotyl_length.setText(str(self.seedObj.hyperCotyl_length_cm))
-            pass
-        except Exception as e:
-            print(e)
-
     def closeEvent(self, event):
         # Perform your desired actions here
         print("Window is being closed...")